838ad952b38d12f308dc9c1b746c9ddc22af4d4c
Jason <jason.novacovici@gmail.com>
2022-08-08 14:40:35 -0400

    updated schemas

---
+++ database/mockData.js
@@ -0,0 +1,66 @@
+{
+  "product_id": "71705",
+  "results": [
+      {
+          "question_id": 631418,
+          "question_body": "What fabric is the bottom made of?",
+          "question_date": "2019-06-28T00:00:00.000Z",
+          "asker_name": "l33tgamer",
+          "question_helpfulness": 3,
+          "reported": false,
+          "answers": {
+              "5897238": {
+                  "id": 5897238,
+                  "body": "The rubber on the bottom wears thin quickly",
+                  "date": "2019-11-28T00:00:00.000Z",
+                  "answerer_name": "n00bgamer",
+                  "helpfulness": 7,
+                  "photos": []
+              },
+              "5897254": {
+                  "id": 5897254,
+                  "body": "Rubber",
+                  "date": "2019-11-28T00:00:00.000Z",
+                  "answerer_name": "n00bgamer",
+                  "helpfulness": 7,
+                  "photos": []
+              },
+              "5987158": {
+                  "id": 5987158,
+                  "body": "does this work",
+                  "date": "2022-07-23T00:00:00.000Z",
+                  "answerer_name": "test",
+                  "helpfulness": 0,
+                  "photos": []
+              }
+          }
+      },
+      {
+          "question_id": 642620,
+          "question_body": "testing",
+          "question_date": "2022-07-30T00:00:00.000Z",
+          "asker_name": "test",
+          "question_helpfulness": 0,
+          "reported": false,
+          "answers": {}
+      },
+      {
+          "question_id": 642619,
+          "question_body": "test",
+          "question_date": "2022-07-30T00:00:00.000Z",
+          "asker_name": "test",
+          "question_helpfulness": 0,
+          "reported": false,
+          "answers": {}
+      },
+      {
+          "question_id": 642615,
+          "question_body": "test",
+          "question_date": "2022-07-30T00:00:00.000Z",
+          "asker_name": "test",
+          "question_helpfulness": 0,
+          "reported": false,
+          "answers": {}
+      }
+  ]
+}
---
+++ database/mongoDB.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/SDC_qna');
+
+const questionSchema = new mongoose.Schema({
+  product_id: Number,
+  question_id: Number, //unique id
+  question_text: String,
+  question_date: String, // timestamp variable?
+  asker_name: String,
+  helpfulness: Number,
+  reported: {type: Boolean, default: false},
+  answers: {
+    answer_id: Number, //unique id
+    answer_text: String,
+    answer_date: String, // timestamp variable?
+    answer_name: String,
+    helpfulness: {type: Number, default: 0},
+    photos: [ { // populate / check for photos upon submission?
+      id: Number,
+      url: String
+    }]
+  }
+});
+
+const answerSchema = new mongoose.Schema({
+  question_id: Number,
+  answer_id: Number, //unique id
+  answer_text: String,
+  answer_date: String, // timestamp variable?
+  answer_name: String,
+  helpfulness: {type: Number, default: 0},
+  photos: [ { // populate / check for photos upon submission?
+    id: Number,
+    url: String
+  }]
+});
+
+const questions = mongoose.model('questions', questionSchema);
+const answers = mongoose.model('answers', answerSchema);
