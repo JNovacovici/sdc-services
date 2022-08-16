8709b42bea9d0b80fbc8de39e55a9b1a1a014bcf
Jason <jason.novacovici@gmail.com>
2022-08-16 13:02:43 -0400

    Added basic route components

--- database/mongoDB.js
+++ database/mongoDB.js
@@ -1,40 +1,48 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 mongoose.connect('mongodb://localhost/SDC_qna');
 
 const questionSchema = new mongoose.Schema({
-  product_id: Number,
   question_id: {type: Number, unique: true}, //unique id
-  question_text: String,
-  question_date: String, // timestamp variable?
+  product_id: Number,
+  body: String,
+  date_written: {type: String, default: new Date().toISOString()}, // timestamp variable?
   asker_name: String,
-  helpfulness: Number,
-  reported: {type: Boolean, default: false},
-  answers: {
-    answer_id: Number, //unique id
-    answer_text: String,
-    answer_date: String, // timestamp variable?
-    answer_name: String,
-    helpfulness: {type: Number, default: 0},
-    photos: [ { // populate / check for photos upon submission?
-      id: Number,
-      url: String
-    }]
-  }
+  asker_email: String,
+  reported: {type: Boolean, default: false}
+  helpful: {type: Number, default: 0}
+});
+
+questionSchema.plugin(AutoIncrement, {
+  inc_field: 'question_id', start_seq: 3518964
 });
 
 const answerSchema = new mongoose.Schema({
   question_id: Number,
   answer_id: {type: Number, unique: true}, //unique id
-  answer_text: String,
-  answer_date: String, // timestamp variable?
-  answer_name: String,
+  body: String,
+  date_written: {type: String, default: new Date().toISOString()}, // timestamp variable?
+  answerer_name: String,
+  answerer_email: String,
   reported: {type: Boolean, default: false},
-  helpfulness: {type: Number, default: 0},
-  photos: [ { // populate / check for photos upon submission?
-    id: Number,
-    url: String
-  }]
+  helpful: {type: Number, default: 0},
+  photos: [] // populate / check for photos upon submission?
+});
+
+answerSchema.plugin(AutoIncrement, {
+  inc_field: 'answer_id', start_seq: 6879307
+});
+
+const photosSchema = new mongoose.Schema({
+  id: {type: Number, unique: true},
+  answer_id: Number,
+  url: String
+})
+
+photosSchema.plugin(AutoIncrement, {
+  inc_field: 'id', start_seq: 2063760
 });
 
 const questions = mongoose.model('questions', questionSchema);
 const answers = mongoose.model('answers', answerSchema);
+const photos = mongoose.model('photos', photosSchema);
--- server/server.js
+++ server/server.js
@@ -2,12 +2,54 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// product id will be req.query.productId
+
 app.get('/qna', (req, res) => {
   res.json({getQnA: 'QnA get test check'});
 });
 
 app.post('/qna', (req, res) => {
   res.json({postQnA: 'QnA post test check'});
+});
+
+/*
+  Question routes
+*/
+
+app.get('/qa/questions', (req, res) => {
+  var id = req.query.productId;
+});
+
+app.post('/qa/questions', (req, res) => {
+
+});
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+
+});
+
+app.put('/qa/questions/:question_id/report', (req, res) => {
+
+});
+
+/*
+  Answer routes
+*/
+
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+
+});
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+
+});
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+
+});
+
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+
 });
 
 module.exports = app;
